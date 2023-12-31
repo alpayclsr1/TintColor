97c5cd8a76e73a9857d377073931e52963475a10
Alpay Calisir <alpay665@hotmail.com>
2023-12-31 15:44:45 +0300

    Add project files.

---
+++ TintColor/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TintColor
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TintColor/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TintColor
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
